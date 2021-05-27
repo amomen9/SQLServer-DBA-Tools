4d0e9161d430e424531cef19ff3ffe1cd3d9e80b
amomen9 <51079948+amomen9@users.noreply.github.com>
2021-05-27 18:09:26 +0430

    Update Backup_SQL_Server_Website.sql

--- Backup_SQL_Server_Website.sql
+++ Backup_SQL_Server_Website.sql
@@ -8,25 +8,8 @@
 
 /*
 
-This script performs a full backup of the database and home folder files of the intended website. It can be turned into a
-scheduled job to run at specific schedules. The DB backup file name will be in DBName_Date_Time + .bak format.
-The home folder backup has a similar name. A checkdb will also be performed prior to the database backup. 
-
-System requirements:
-SQL Server Compatibility: This script is designed to comply with SQL Server 2008 R2 and later. Earlier versions are not tested.
-This script utilizes 7zip version 19.0, so install 7-zip first, which is an open source and multiplatform compression software.
-Sample 7zip commands
-	for compression:
-	7z a -tzip -mx9 -mmt4 -y -bd -ssw -stl  -p1234 "D:\Website Backup\21.03.10_0500\DBNAME_File Backup_21.03.10_0500.zip" "C:\inetpub\wwwroot\*"
-	for extraction:
-	7z x -aoa -spe -p1234 -o"C:\inetpub\wwwroot" "D:\Website Backup\21.03.10_0500\DBNAME_File Backup_21.03.10_0500.zip"
-For information regarding 7zip commands and switches please refer to 7zip's manual.
-
-This script also checks for the modification of Database's data files on SQL Server 2016 and later. If the modification is less than 60 pages,
-no backup is performed. This number is experimental. To backup regardless, simply set threshold to 0. There will be
-only one output file for database backup. For security reasons, the script enables the extended stored procedure xp_cmdshell
-and disables it again immediately once the procedure is finished executing. Using website files' archive password is
-recommended and this script does not offer an option not to set a password.
+This script performs a full backup of the database and home folder files of the intended website. For full guide please refer to
+the README.md file beside this file on the repository.
 
 Attention: 
 	1. This script does not backup home folder on Non-Windows host operating systems, as xp_cmdshell is only
