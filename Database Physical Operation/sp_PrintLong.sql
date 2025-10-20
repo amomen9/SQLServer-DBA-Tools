a0390c5b9b41564d029c0274842ff6e982849a40
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-20 19:57:14 +0330

    Create sp_PrintLong.sql

---
+++ Database Physical Operation/sp_PrintLong.sql
@@ -0,0 +1,58 @@
+﻿--USE master
+--DROP PROC dbo.sp_PrintLong
+
+USE master
+GO
+
+
+
+CREATE OR ALTER PROC sp_PrintLong
+@String NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @NewLineLocation INT,
+			@TempStr NVARCHAR(4000),
+			@Length INT,
+			@carriage BIT
+
+	WHILE @String <> ''
+	BEGIN
+		IF LEN(@String)<=4000
+		BEGIN 
+			PRINT @String
+			BREAK
+		END 
+		ELSE
+        BEGIN 
+			SET @TempStr = SUBSTRING(@String,1,4000)
+			SELECT @NewLineLocation = CHARINDEX(CHAR(10),REVERSE(@TempStr))
+
+			IF CHARINDEX(CHAR(13),REVERSE(@TempStr)) - @NewLineLocation = 1
+				SET @carriage = 1
+			ELSE
+				SET @carriage = 0
+
+			SET @TempStr = LEFT(@TempStr,(4000-@NewLineLocation)-CONVERT(INT,@carriage))
+
+			PRINT @TempStr
+		
+			SET @Length = LEN(@String)-LEN(@TempStr)-CONVERT(INT,@carriage)-1
+			SET @String = RIGHT(@String,@Length)
+			
+		END 
+	END
+END
+GO
+
+--============================================================================================
+
+DECLARE @def NVARCHAR(MAX)
+--SELECT @def=definition FROM sys.all_sql_modules WHERE OBJECT_ID > 0 AND LEN(definition) = (SELECT MAX(LEN(definition)) FROM sys.all_sql_modules WHERE OBJECT_ID > 0)
+SELECT TOP 1 @def = definition FROM sys.all_sql_modules ORDER BY LEN(definition) DESC
+PRINT (LEN(@def))
+EXEC dbo.sp_PrintLong @String = @def -- nvarchar(max)
+
+
+DROP PROC dbo.sp_PrintLong
+GO
+
