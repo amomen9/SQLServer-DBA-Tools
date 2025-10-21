8d2f9a9b41000d11d80b6789c8389cd567acbfe2
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-21 17:33:18 +0330

    Update hard kill.sql

--- Database & Disk Physical Operation/hard kill.sql
+++ Database & Disk Physical Operation/hard kill.sql
@@ -1,3 +1,13 @@
+-- =============================================
+-- Author:              "a-momen"
+-- Contact & Report:    "amomen@gmail.com"
+-- Update date:         "2025-09-15"
+-- Description:         "hard kill"
+-- License:             "Please refer to the license file"
+-- =============================================
+
+
+
 USE master
 GO
 
