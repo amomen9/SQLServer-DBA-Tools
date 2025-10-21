0555152a5763fc26a209888e615bd274302f2192
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-21 17:32:38 +0330

    Update Stored Procedures.sql

--- BI/Northwind Data Warehouse Project/Scripts/Stored Procedures.sql
+++ BI/Northwind Data Warehouse Project/Scripts/Stored Procedures.sql
@@ -1,3 +1,13 @@
+-- =============================================
+-- Author:              "a-momen"
+-- Contact & Report:    "amomen@gmail.com"
+-- Update date:         "2022-05-12"
+-- Description:         "Stored Procedures"
+-- License:             "Please refer to the license file"
+-- =============================================
+
+
+
 
 
 --SELECT [OrderID]
