28002c88449d84f1b8a36b0c121da9afdd34c841
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-21 17:34:55 +0330

    Update CPU intensive tasks for an instance (dbWarden).sql

--- Monitoring/dbWarden/CPU intensive tasks for an instance (dbWarden).sql
+++ Monitoring/dbWarden/CPU intensive tasks for an instance (dbWarden).sql
@@ -1,3 +1,13 @@
+-- =============================================
+-- Author:              "a-momen"
+-- Contact & Report:    "amomen@gmail.com"
+-- Update date:         "2022-08-26"
+-- Description:         "CPU intensive tasks for an instance (dbWarden)"
+-- License:             "Please refer to the license file"
+-- =============================================
+
+
+
 
 ;WITH dt AS
 (
