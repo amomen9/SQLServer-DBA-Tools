251711953bcc4c4ad3c0ba860ec84de0f64d07fe
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-21 17:35:32 +0330

    Update Last run of every job.sql

--- msdb (Except Backup & Restore)/Last run of every job.sql
+++ msdb (Except Backup & Restore)/Last run of every job.sql
@@ -1,3 +1,13 @@
+-- =============================================
+-- Author:              "a-momen"
+-- Contact & Report:    "amomen@gmail.com"
+-- Update date:         "2022-09-24"
+-- Description:         "Last run of every job"
+-- License:             "Please refer to the license file"
+-- =============================================
+
+
+
 
 SELECT * FROM 
 (
