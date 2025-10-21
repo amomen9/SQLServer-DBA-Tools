e0beb6dc92087ce39afca81db7a06b003736523f
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-21 10:35:17 +0330

    Create kill particular running jobs.sql

---
+++ msdb (Except Backup & Restore)/kill particular running jobs.sql
@@ -0,0 +1,38 @@
+-------------------------- Using kill
+DECLARE @SQL NVARCHAR(max)
+
+SELECT @SQL= STRING_AGG('KILL '+dt.session_id+'; --JobName: '+dt.[Job Name],CHAR(10))
+FROM
+(
+	SELECT
+		CONVERT(VARCHAR,s.session_id) session_id,
+		(SELECT name FROM msdb..sysjobs WHERE job_id=(SELECT CONVERT(VARCHAR(100),CONVERT(UNIQUEIDENTIFIER,CONVERT(VARBINARY(100), SUBSTRING(program_name,30,34),1))) FROM sys.dm_exec_sessions WHERE session_id=s.session_id)) [Job Name]
+	FROM
+	sys.dm_exec_sessions s 
+	WHERE s.program_name LIKE 'SQLAgent - TSQL JobStep (Job %'
+) dt
+WHERE dt.[Job Name] LIKE '%IndexOptimize%' OR dt.[Job Name] LIKE '%UpdateStatistics%'
+
+PRINT @SQL
+EXEC(@SQL)
+
+-------------------------- Using sp_stop_job
+
+DECLARE @SQL NVARCHAR(max)
+
+SELECT @SQL= STRING_AGG('exec msdb.dbo.sp_stop_job @job_name='''+dt.[Job Name]+'''',CHAR(10))
+FROM
+(
+	SELECT
+		CONVERT(VARCHAR,s.session_id) session_id,
+		(SELECT name FROM msdb..sysjobs WHERE job_id=(SELECT CONVERT(VARCHAR(100),CONVERT(UNIQUEIDENTIFIER,CONVERT(VARBINARY(100), SUBSTRING(program_name,30,34),1))) FROM sys.dm_exec_sessions WHERE session_id=s.session_id)) [Job Name]
+	FROM
+	sys.dm_exec_sessions s 
+	WHERE s.program_name LIKE 'SQLAgent - TSQL JobStep (Job %'
+) dt
+WHERE dt.[Job Name] LIKE '%IndexOptimize%' OR dt.[Job Name] LIKE '%UpdateStatistics%'
+
+PRINT @SQL
+EXEC(@SQL)
+
+
