3ef930f6c9725face11524856e6fc55df594510b
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-10-26 12:54:59 +0330

    Change SQL Server port from 55555 to 2828 in firewall script
    Updated the SQL Server registry and firewall rules to use port 2828 instead of 55555 for both inbound and outbound connections. This ensures the server listens and allows traffic on the new port.

--- Installation & Configuration/SQL Server Unattended (Silent) Installation/SQL port-firewall.bat
+++ Installation & Configuration/SQL Server Unattended (Silent) Installation/SQL port-firewall.bat
@@ -1,9 +1,9 @@
-reg add "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" /t REG_SZ /v TcpPort /d 55555 /f
+reg add "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" /t REG_SZ /v TcpPort /d 2828 /f
 
 powershell New-NetFirewallRule -DisplayName "AGPort" -Direction Inbound -LocalPort 5022 -Protocol TCP -Action Allow
 powershell New-NetFirewallRule -DisplayName "AGPort" -Direction Outbound -LocalPort 5022 -Protocol TCP -Action Allow
 
-powershell New-NetFirewallRule -DisplayName "SQLPort" -Direction Inbound -LocalPort 55555 -Protocol TCP -Action Allow
-powershell New-NetFirewallRule -DisplayName "SQLPort" -Direction Outbound -LocalPort 55555 -Protocol TCP -Action Allow
+powershell New-NetFirewallRule -DisplayName "SQLPort" -Direction Inbound -LocalPort 2828 -Protocol TCP -Action Allow
+powershell New-NetFirewallRule -DisplayName "SQLPort" -Direction Outbound -LocalPort 2828 -Protocol TCP -Action Allow
 
 net stop mssqlserver /Y && net start mssqlserver && net start sqlserveragent
