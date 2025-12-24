91b8234f436ce6ffa1fa750f0a182c4ad43893fb
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-12-24 12:17:51 +0330

    Refactor script into stored procedure for DB attach
    Converted the standalone SQL script for attaching detached databases into a parameterized stored procedure (usp_attach_detached_databases). This improves reusability, encapsulates logic, and allows specifying the search path as a parameter. Also added an example EXEC call for the new procedure.

--- Database & Disk Physical Operation/Database/attach dettached databases.sql
+++ Database & Disk Physical Operation/Database/attach dettached databases.sql
@@ -8,105 +8,134 @@
 --                      error handling with optional PRINT vs RAISERROR output mode.
 -- =============================================
 
-SET NOCOUNT ON;
 
------------ Parameter -------------------------------------------------------
--- Define your search path for mdf files here. If not specified, the instance default data path will be used
-DECLARE @Search_Path NVARCHAR(256) 
------------------------------------------------------------------------------
-
-
--- Drop leftover temp tables from prior runs
-DROP TABLE IF EXISTS #DataFilePaths;
-DROP TABLE IF EXISTS #DBDetails;
-
--- Working variables
-DECLARE @path              NVARCHAR(255);
-DECLARE @SQL               NVARCHAR(MAX);
-DECLARE @PRINT_or_RAISERROR INT = 2;            -- 1 = PRINT errors, 2 = RAISERROR (default)
-DECLARE @ErrMsg            NVARCHAR(500);
-DECLARE @ErrLine           NVARCHAR(10);
-DECLARE @ErrNo             NVARCHAR(6);
-DECLARE @ErrState          NVARCHAR(3);
-DECLARE @ErrSeverity       NVARCHAR(2);
-DECLARE @UDErrMsg          NVARCHAR(MAX);
-
--- Temp table to hold file list returned by DBCC CHECKPRIMARYFILE (option 3)
-CREATE TABLE #DataFilePaths
-(
-    id       INT NOT NULL IDENTITY PRIMARY KEY,
-    [status] INT,
-    [fileid] SMALLINT,
-    [name]   NCHAR(128),
-    [filename] NCHAR(260)
-);
-
--- Temp table to hold database properties returned by DBCC CHECKPRIMARYFILE (option 2)
-CREATE TABLE #DBDetails
-(
-    id INT NOT NULL IDENTITY PRIMARY KEY,
-    [property]   NVARCHAR(128),
-    [value_sqlv] SQL_VARIANT,
-    [value]      AS CONVERT(SYSNAME, value_sqlv)
-);
-
-IF (ISNULL(@Search_Path,'')='') SET @Search_Path = CONVERT(NVARCHAR(256),SERVERPROPERTY('InstanceDefaultDataPath'))
--- Cursor enumerates *.mdf files under specified root path. Define arbitrary root path per
--- your needs
-DECLARE Attacher CURSOR FAST_FORWARD FOR
-    SELECT full_filesystem_path
-    FROM  
-    sys.dm_os_enumerate_filesystem(@Search_Path, N'*.mdf') fs
-    LEFT JOIN sys.master_files mf
-    ON mf.physical_name = fs.full_filesystem_path
-    WHERE mf.database_id IS NULL AND fs.file_or_directory_name NOT IN ('model_msdbdata.mdf','model_replicatedmaster.mdf','mssqlsystemresource.mdf')
-
-OPEN Attacher;
-
-FETCH NEXT FROM Attacher INTO @path;
-WHILE @@FETCH_STATUS = 0
+CREATE OR ALTER PROCEDURE usp_attach_detached_databases 
+    ----------- Parameter -------------------------------------------------------
+    -- Define your search path for mdf files here. If not specified, the instance default data path will be used
+    @Search_Path  NVARCHAR(256) = ''
+    -----------------------------------------------------------------------------
+AS
 BEGIN
-    -- Clean temp tables for this iteration
-    TRUNCATE TABLE #DataFilePaths;
-    TRUNCATE TABLE #DBDetails;
 
-    BEGIN TRY
-        -- Retrieve file metadata (option 3)
-        SET @SQL = N'DBCC CHECKPRIMARYFILE (N''' + @path + N''', 3) WITH NO_INFOMSGS;';
-        INSERT #DataFilePaths
-        EXEC (@SQL);
+    SET NOCOUNT ON;
 
-        -- Retrieve database name & other properties (option 2)
-        SET @SQL = N'DBCC CHECKPRIMARYFILE (N''' + @path + N''', 2) WITH NO_INFOMSGS;';
-        INSERT #DBDetails
-        EXEC (@SQL);
+    -- Drop leftover temp tables from prior runs
+    DROP TABLE IF EXISTS #DataFilePaths;
+    DROP TABLE IF EXISTS #DBDetails;
+
+    -- Working variables
+    DECLARE @path              NVARCHAR(255);
+    DECLARE @SQL               NVARCHAR(MAX);
+    DECLARE @PRINT_or_RAISERROR INT = 2;            -- 1 = PRINT errors, 2 = RAISERROR (default)
+    DECLARE @ErrMsg            NVARCHAR(500);
+    DECLARE @ErrLine           NVARCHAR(10);
+    DECLARE @ErrNo             NVARCHAR(6);
+    DECLARE @ErrState          NVARCHAR(3);
+    DECLARE @ErrSeverity       NVARCHAR(2);
+    DECLARE @UDErrMsg          NVARCHAR(MAX);
+
+    -- Temp table to hold file list returned by DBCC CHECKPRIMARYFILE (option 3)
+    CREATE TABLE #DataFilePaths
+    (
+        id       INT NOT NULL IDENTITY PRIMARY KEY,
+        [status] INT,
+        [fileid] SMALLINT,
+        [name]   NCHAR(128),
+        [filename] NCHAR(260)
+    );
+
+    -- Temp table to hold database properties returned by DBCC CHECKPRIMARYFILE (option 2)
+    CREATE TABLE #DBDetails
+    (
+        id INT NOT NULL IDENTITY PRIMARY KEY,
+        [property]   NVARCHAR(128),
+        [value_sqlv] SQL_VARIANT,
+        [value]      AS CONVERT(SYSNAME, value_sqlv)
+    );
+
+    IF (ISNULL(@Search_Path,'')='') SET @Search_Path = CONVERT(NVARCHAR(256),SERVERPROPERTY('InstanceDefaultDataPath'))
+    -- Cursor enumerates *.mdf files under specified root path. Define arbitrary root path per
+    -- your needs
+    DECLARE Attacher CURSOR FAST_FORWARD FOR
+        SELECT full_filesystem_path
+        FROM  
+        sys.dm_os_enumerate_filesystem(@Search_Path, N'*.mdf') fs
+        LEFT JOIN sys.master_files mf
+        ON mf.physical_name = fs.full_filesystem_path
+        WHERE mf.database_id IS NULL AND fs.file_or_directory_name NOT IN ('model_msdbdata.mdf','model_replicatedmaster.mdf','mssqlsystemresource.mdf')
+
+    OPEN Attacher;
+
+    FETCH NEXT FROM Attacher INTO @path;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Clean temp tables for this iteration
+        TRUNCATE TABLE #DataFilePaths;
+        TRUNCATE TABLE #DBDetails;
 
         BEGIN TRY
-            -- Build parameter list for sys.sp_attach_db (filename arguments)
-            SELECT @SQL =
-                STRING_AGG(REPLICATE(CHAR(9), 7) + N'@filename' + CONVERT(NVARCHAR(10), id) +
-                           N' = N''' + TRIM(filename) + N'''', N',' + CHAR(10))
-            FROM #DataFilePaths;
+            -- Retrieve file metadata (option 3)
+            SET @SQL = N'DBCC CHECKPRIMARYFILE (N''' + @path + N''', 3) WITH NO_INFOMSGS;';
+            INSERT #DataFilePaths
+            EXEC (@SQL);
 
-            -- Prepend EXEC line with @dbname
-            SET @SQL =
-                  CHAR(9) + N'EXEC sys.sp_attach_db ' +
-                  N'@dbname = N''' + (SELECT value FROM #DBDetails WHERE property = N'Database name') + N''',' + CHAR(10) +
-                  @SQL;
+            -- Retrieve database name & other properties (option 2)
+            SET @SQL = N'DBCC CHECKPRIMARYFILE (N''' + @path + N''', 2) WITH NO_INFOMSGS;';
+            INSERT #DBDetails
+            EXEC (@SQL);
 
-            PRINT @SQL;        -- Echo generated command
-            EXEC (@SQL);       -- Execute attach
+            BEGIN TRY
+                -- Build parameter list for sys.sp_attach_db (filename arguments)
+                SELECT @SQL =
+                    STRING_AGG(REPLICATE(CHAR(9), 7) + N'@filename' + CONVERT(NVARCHAR(10), id) +
+                               N' = N''' + TRIM(filename) + N'''', N',' + CHAR(10))
+                FROM #DataFilePaths;
+
+                -- Prepend EXEC line with @dbname
+                SET @SQL =
+                      CHAR(9) + N'EXEC sys.sp_attach_db ' +
+                      N'@dbname = N''' + (SELECT value FROM #DBDetails WHERE property = N'Database name') + N''',' + CHAR(10) +
+                      @SQL;
+
+                PRINT @SQL;        -- Echo generated command
+                EXEC (@SQL);       -- Execute attach
+            END TRY
+            BEGIN CATCH
+                -- Handle inner attach errors
+                SET @ErrMsg     = ERROR_MESSAGE();
+                SET @ErrLine    = CONVERT(NVARCHAR(10), ERROR_LINE());
+                SET @ErrNo      = CONVERT(NVARCHAR(6), ERROR_NUMBER());
+                SET @ErrState   = CONVERT(NVARCHAR(3), ERROR_STATE());
+                SET @ErrSeverity= CONVERT(NVARCHAR(2), ERROR_SEVERITY());
+
+                SET @UDErrMsg =
+                    N'Attach operation failed for file: ' + @path + CHAR(10) +
+                    N'System error:' + CHAR(10) +
+                    N'Msg ' + @ErrNo + N', Level ' + @ErrSeverity + N', State ' + @ErrState +
+                    N', Line ' + @ErrLine + CHAR(10) + @ErrMsg;
+
+                IF @PRINT_or_RAISERROR = 1
+                BEGIN
+                    PRINT @UDErrMsg;
+                    PRINT CHAR(13) + REPLICATE('-', 108);
+                END
+                ELSE
+                BEGIN
+                    PRINT CHAR(13) + REPLICATE('-', 108);
+                    RAISERROR (@UDErrMsg, 16, 1);
+                END
+            END CATCH
         END TRY
         BEGIN CATCH
-            -- Handle inner attach errors
-            SET @ErrMsg     = ERROR_MESSAGE();
-            SET @ErrLine    = CONVERT(NVARCHAR(10), ERROR_LINE());
-            SET @ErrNo      = CONVERT(NVARCHAR(6), ERROR_NUMBER());
-            SET @ErrState   = CONVERT(NVARCHAR(3), ERROR_STATE());
-            SET @ErrSeverity= CONVERT(NVARCHAR(2), ERROR_SEVERITY());
+            -- Handle metadata extraction errors
+            SET @ErrMsg      = ERROR_MESSAGE();
+            SET @ErrLine     = CONVERT(NVARCHAR(10), ERROR_LINE());
+            SET @ErrNo       = CONVERT(NVARCHAR(6), ERROR_NUMBER());
+            SET @ErrState    = CONVERT(NVARCHAR(3), ERROR_STATE());
+            SET @ErrSeverity = CONVERT(NVARCHAR(2), ERROR_SEVERITY());
 
             SET @UDErrMsg =
-                N'Attach operation failed for file: ' + @path + CHAR(10) +
+                N'Pre-attach metadata collection failed for file: ' + @path + CHAR(10) +
                 N'System error:' + CHAR(10) +
                 N'Msg ' + @ErrNo + N', Level ' + @ErrSeverity + N', State ' + @ErrState +
                 N', Line ' + @ErrLine + CHAR(10) + @ErrMsg;
@@ -121,37 +150,15 @@ BEGIN
                 PRINT CHAR(13) + REPLICATE('-', 108);
                 RAISERROR (@UDErrMsg, 16, 1);
             END
-        END CATCH
-    END TRY
-    BEGIN CATCH
-        -- Handle metadata extraction errors
-        SET @ErrMsg      = ERROR_MESSAGE();
-        SET @ErrLine     = CONVERT(NVARCHAR(10), ERROR_LINE());
-        SET @ErrNo       = CONVERT(NVARCHAR(6), ERROR_NUMBER());
-        SET @ErrState    = CONVERT(NVARCHAR(3), ERROR_STATE());
-        SET @ErrSeverity = CONVERT(NVARCHAR(2), ERROR_SEVERITY());
+        END CATCH;
 
-        SET @UDErrMsg =
-            N'Pre-attach metadata collection failed for file: ' + @path + CHAR(10) +
-            N'System error:' + CHAR(10) +
-            N'Msg ' + @ErrNo + N', Level ' + @ErrSeverity + N', State ' + @ErrState +
-            N', Line ' + @ErrLine + CHAR(10) + @ErrMsg;
+        -- Advance cursor
+        FETCH NEXT FROM Attacher INTO @path;
+    END;
 
-        IF @PRINT_or_RAISERROR = 1
-        BEGIN
-            PRINT @UDErrMsg;
-            PRINT CHAR(13) + REPLICATE('-', 108);
-        END
-        ELSE
-        BEGIN
-            PRINT CHAR(13) + REPLICATE('-', 108);
-            RAISERROR (@UDErrMsg, 16, 1);
-        END
-    END CATCH;
+    CLOSE Attacher;
+    DEALLOCATE Attacher;
+END
+GO
 
-    -- Advance cursor
-    FETCH NEXT FROM Attacher INTO @path;
-END;
-
-CLOSE Attacher;
-DEALLOCATE Attacher;
+EXEC dbo.usp_attach_detached_databases @Search_Path = N'' -- nvarchar(256)
